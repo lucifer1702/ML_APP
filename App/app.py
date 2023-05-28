19e02517025535697c583276cd33c5ed7bba7cf0
Mukund <mukund@Mukunds-MacBook-Air.local>
2023-05-29 00:47:31 +0530

    This app is not the best but its my first.. hoping to build more

--- App/app.py
+++ App/app.py
@@ -1,12 +1,50 @@
 import streamlit
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import joblib
 
-def main():
-    menu=["home","Monitor","about"]
-    choice=streamlit.sidebar.selectbox("Menu",menu)
-    
+pipe=joblib.load(open("/Users/mukund/Desktop/IIT_KGP/MLDL/ML_APP/App/models/piped_file_emotions.pkl",'rb'))
 
-if __name__=='__main__':
-    main()    
+def predict_results(docs):
+    result=pipe.predict([docs])
+    return result[0]
+def get_prediction(docs):
+    results=pipe.predict_proba([docs])
+    return results
+
+
+def main():
+    streamlit.title("emotion classifier app")
+    menu = ["home", "Monitor", "about"]
+    choice = streamlit.sidebar.selectbox("Menu", menu)
+    if choice == "home":
+        streamlit.subheader("QUERY  ")
+        with streamlit.form(key='emotion-detect'):
+            raw_text = streamlit.text_area("type-here")
+            sub_text = streamlit.form_submit_button(label='YES')
+        if sub_text:
+            col1, col2 = streamlit.columns(2)
+            prediction=predict_results(raw_text)
+            probability=get_prediction(raw_text)
+
+
+            with col1:
+                streamlit.success("original text")
+                streamlit.write(raw_text)
+                streamlit.success("prediction")
+                streamlit.write(prediction)
+                streamlit.write("confidence  : {}" .format(np.max(probability)))
+            with col2:
+                streamlit.success("probability of prediction")
+                streamlit.write(probability)
+
+    elif choice == "Monitor":
+        streamlit.subheader("monitor-app")
+        streamlit.write('this part will be updated later to monitor the status of the website and other cool stuff')
+    else:
+        streamlit.subheader("About")
+        streamlit.write("this is me Mukundan . Writing this, I feel happy That this is the first website I built with STREAMLIT AND MY FIRST ML POWERED WEBSITE")
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -8,4 +8,17 @@ The app will contain 3 directories :
 2. Dataset Folder which will contain the dataset
 3. The notebook folder will contain all the notebooks which will be used for the training of the ML model
 
-Since the accuracy of classical Ml models were really poor in the case of both tfidf and count vectorizer I decided to use DL Models . But due some dependancy issues I am currently unable to use them so we would be sticking with Ml models for now . Will update it later in the future with DL models
+Since the accuracy of classical Ml models were really poor in the case of both tfidf and count vectorizer I decided to use DL Models . But due some dependancy issues I am currently unable to use them so we would be sticking with Ml models for now . Will update it later in the future with DL mode
+
+
+To run the app 
+
+   1.Clone the repo using git clone
+
+2. cd into the directory ML_app
+3. then run the command
+
+   ```
+   streamlit run App/app.py
+   ```
+   4. The website must run on your local host
