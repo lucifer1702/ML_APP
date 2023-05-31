957b8bc2515a3742b152c2496146cbff7627151f
Mukund <mukund@Mukunds-MacBook-Air.local>
2023-06-01 01:44:40 +0530

    commit

--- App/app.py
+++ App/app.py
@@ -2,33 +2,37 @@ import streamlit
 import numpy as np
 import pandas as pd
 import joblib
-import altair as alt
 from keras.utils import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model
+from sklearn.ensemble import VotingClassifier
+import plotly.figure_factory as ff
 
-model=load_model('App/models/model_lstm.h5')
+model1=load_model('App/models/model_lstm.h5 1.42.50 AM')
+model2=joblib.load('App/models/piped_file_emotions.pkl')
 
-class_dict={0:'anger', 1:'boredom', 2:'empty', 3:'enthusiasm', 4:'fun', 5:'happiness', 6:'hate',
-       7:'love', 8:'neutral', 9:'relief',10: 'sadness', 11:'surprise', 12:'worry'
-}
 
-max_fatures = 5000
 
-def tokenf(X):
-    token=Tokenizer(split=' ',num_words=max_fatures)
-    token.fit_on_texts([X])
-    X = token.texts_to_sequences([X])
-    X = pad_sequences(X,maxlen=80)
+cols=['anger', 'boredom', 'empty', 'enthusiasm', 'fun', 'happiness', 'hate',
+       'love', 'neutral', 'relief', 'sadness', 'surprise', 'worry']
 
-    return X
+def tokenf(sample):
+    sample=pd.Series(sample)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(sample)
+    word_indices = tokenizer.texts_to_sequences(sample)
+    word_index = tokenizer.word_index
+    x_sample=pad_sequences(word_indices,maxlen=100)
+    return x_sample
+
+
 
 
 def predict_results(docs):
     docs= tokenf(docs)
-    result=model.predict(docs)
-   
-    return result
+    ##voting ensemble 
+    result=model1.predict(docs)
+    return result 
 
 
 
@@ -37,15 +41,16 @@ def main():
     menu = ["home", "Monitor", "about"]
     choice = streamlit.sidebar.selectbox("Menu", menu)
     if choice == "home":
-        streamlit.subheader("QUERY  ")
+        streamlit.subheader("QUERY")
         with streamlit.form(key='emotion-detect'):
             raw_text = streamlit.text_area("type-here")
             sub_text = streamlit.form_submit_button(label='YES')
         if sub_text:
             col1, col2 = streamlit.columns(2)
             prediction=predict_results(raw_text)
-            emotion=class_dict[np.argmax(prediction)]
+            emotion=cols[np.argmax(prediction)]
             probability=np.max(predict_results(raw_text))
+           
 
 
             with col1:
@@ -56,13 +61,13 @@ def main():
                 streamlit.write("confidence  : {}" .format(np.max(probability)))
             with col2:
                 streamlit.success("probability of each class prediction")
-                streamlit.write(probability)
-                proba_df =pd.DataFrame(prediction ,columns=class_dict.values)
+                # streamlit.write(probability)
+                proba_df =pd.DataFrame(prediction ,columns=cols)
                 streamlit.write(proba_df)
+                # fig = ff.create_distplot(prediction,cols, bin_size=[.1, .25, .5])
+                # streamlit.plotly_chart(fig, use_container_width=True)
 
-                fig =alt.Chart(proba_df).mark_bar().encode(x='Emotions',y='probability',color='Emotions')
-                streamlit.altair_chart(fig)
-
+                
     elif choice == "Monitor":
         streamlit.subheader("monitor-app")
         streamlit.write('this part will be updated later to monitor the status of the website and other cool stuff')
