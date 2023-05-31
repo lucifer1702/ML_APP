3f7f6d0a01d8295372dea5329518a3be49a3355c
Mukund <mukund@Mukunds-MacBook-Air.local>
2023-06-01 02:05:46 +0530

    0

--- App/app.py
+++ App/app.py
@@ -4,11 +4,11 @@ import pandas as pd
 import joblib
 
 # import keras
-from keras.utils import pad_sequences
+from keras.preprocessing.sequence import pad_sequences
+
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model
-from sklearn.ensemble import VotingClassifier
-import plotly.figure_factory as ff
+
 
 model1=load_model('App/models/model_lstm.h5')
 model2=joblib.load('App/models/piped_file_emotions.pkl')
